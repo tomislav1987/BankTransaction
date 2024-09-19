ef42f641d93bb1b79570a77ec17b43b2336a580e
Tomislav Pavic <t.pavic@outlook.com>
2024-09-19 06:13:00 +0200

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransaction.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName ="nvarchar(12)")]
+        [DisplayName("Broj Računa")]
+        [Required(ErrorMessage="Ovo polje je obavezno !")]
+        [MaxLength(12, ErrorMessage ="Maksimum 12 znakova")]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Ime Klijenta")]
+        [Required(ErrorMessage ="Ovo polje je obavezno !.")]
+        public string BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Ime Banke")]
+        [Required(ErrorMessage = "Ovo polje je obavezno")]
+        public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage ="Ovo polje je obavezno")]
+        [MaxLength(11, ErrorMessage ="Maksimum 11 znakova")]
+        public string SWIFTCode { get; set; }
+		[DisplayName("Iznos")]
+		public int Amount { get; set; }
+
+		[DisplayName("Nadnevak")]
+		[DisplayFormat(DataFormatString ="{0:MMM-dd-yy}")]
+        public DateTime Date {  get; set; }
+    }
+}
